ba0e3dc7210b7a7393645bfae7aa37eaa7012e09
nathanwn <49882031+nathanwn@users.noreply.github.com>
2024-08-30 19:14:03 +1000

    update

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,8 @@
-# if (Get-Command "oh-my-posh" -errorAction SilentlyContinue) {
-#     # oh-my-posh init pwsh --config "$HOME\AppData\Local\Programs\oh-my-posh\themes\pure.omp.json" | Invoke-Expression
-#     oh-my-posh init pwsh --config "$HOME\.config\oh-my-posh\config.yaml" | Invoke-Expression
-#     oh-my-posh disable notice
-# } else {
-#     echo "oh-my-posh is not installed"
-# }
-
-function prompt {"$PWD`n> "}
+if (Get-Command "starship" -ErrorAction SilentlyContinue) {
+    Invoke-Expression (&starship init powershell)
+} else {
+    function prompt {"$PWD`n> "}
+}
 
 function which($name) {
     Get-Command $name | Select-Object -ExpandProperty Definition
@@ -14,7 +10,7 @@ function which($name) {
 
 # Override cd
 # cd is aliased to Set-Location by default
-if (Get-Alias -name cd -errorAction SilentlyContinue) {
+if (Get-Alias -name cd -ErrorAction SilentlyContinue) {
     Remove-Alias cd
 }
 
@@ -22,19 +18,38 @@ function cd($name) {
     Set-Location $name
     if (Test-Path ".envrc.ps1") {
         echo "Detected .envrc.ps1. Executing it now..."
-        .\.envrc.ps1
+        . .\.envrc.ps1
     }
 }
+
+function cdf($name) {
+    if (Get-Command "fzf" -ErrorAction SilentlyContinue) {
+        $repo_dir = Get-ChildItem -Path "$env:USERPROFILE\dev" -Force  -Recurse -Directory -Depth 4 -Filter ".git" | ForEach-Object { $_.Parent.Fullname } | fzf
+        cd $repo_dir
+    } else {
+        echo "ERROR: fzf not found"
+    }
+}
+
 
 function tail($filename) {
     Get-Content $filename -Wait -Tail 30
 }
 
+function wsl-shutdown() {
+    wsl --shutdown --system
+}
+
+# Environment variables
 $env:EDITOR="nvim"
 $env:XDG_CONFIG_HOME = "${HOME}\.config"
 $env:XDG_DATA_HOME = "${HOME}\.local\share"
 $env:XDG_STATE_HOME = "${HOME}\.local\state"
 $env:XDG_CACHE_HOME = "${HOME}\.cache"
+
+# Aliases
+Set-Alias -Name vim -Value nvim
+Set-Alias -Name open -Value Start-Process
 
 # Load local config
 $local_config_file = Join-Path -Path $PSScriptRoot -ChildPath "local.ps1"
@@ -42,4 +57,34 @@ $local_config_file = Join-Path -Path $PSScriptRoot -ChildPath "local.ps1"
 if (Test-Path $local_config_file) {
     Write-Output "Loading ${local_config_file}"
     . $local_config_file
+    if (Test-Path ".envrc.ps1") {
+        echo "Detected .envrc.ps1. Executing it now..."
+        . .\.envrc.ps1
+    }
 }
+
+# Color groups:
+# - Command
+# - Comment
+# - ContinuationPrompt
+# - Default
+# - Emphasis
+# - Error
+# - InlinePrediction
+# - Keyword
+# - ListPrediction
+# - ListPredictionSelected
+# - ListPredictionTooltip
+# - Member
+# - Number
+# - Operator
+# - Parameter
+# - Selection
+# - String
+# - Type
+# - Variable
+Set-PSReadLineOption -Colors @{ InlinePrediction = '#A0A0A0' }
+Set-PSReadLineOption -Colors @{ Parameter = '#404040' }
+Set-PSReadLineOption -Colors @{ Command = '#006400' }
+
+Set-PSReadLineKeyHandler -Chord "Ctrl+y" -Function ForwardWord
